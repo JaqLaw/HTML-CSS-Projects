8e7abd051ea64c262c3c271a368fcb7231e6a57a
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2021-11-26 21:12:47 -0800

    som updates sea creatures

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -3,3 +3,22 @@ function My_First_Function() {
     var result = String.fontcolor("green");
     document.getElementById("Irish").innerHTML = result;
 }
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    let y = document.forms["myForm"]["lname"].value;
+    let z = document.forms["myForm"]["phone"].value;
+    if (x == "" || y == "" || z == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+}
+
+
+function openForm() {
+    document.getElementById("theForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("theForm").style.display = "none";
+  }
